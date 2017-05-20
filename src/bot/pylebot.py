0edf8269242e5c3c7af3a4ce92392e4a390b8414
tiago.paranhos <tiagoprn@gmail.com>
2017-05-20 20:29:02 -0300

    Unifying code to refactor later.

--- src/bot/pylebot.py
+++ src/bot/pylebot.py
@@ -3,13 +3,10 @@ import sys
 
 import telepot
 
+from datetime import datetime
+from urllib.parse import urlparse
+
 from telepot.delegate import pave_event_space, per_chat_id, create_open
-
-if __name__ == '__main__':
-    from commands import Command
-else:
-    from .commands import Command
-
 
 """
 You must register this bot with BotFather. 
@@ -18,9 +15,15 @@ Then, create a token for it and set as an environment variable:
 
     $ export PYLE_BOT_TOKEN=your-bot-token-here
     
-Reference: http://telepot.readthedocs.io/en/latest/    
+References: 
+http://telepot.readthedocs.io/en/latest/
+https://github.com/nickoala/telepot/blob/master/test/test3_send.py
+https://core.telegram.org/bots/api#sendmessage
+https://core.telegram.org/bots/api/#keyboardbutton    
 """
-
+AVAILABLE_COMMANDS = [
+    '/hi'  # Greets the user with the current time
+]
 BOT_TOKEN = os.environ.get('PYLE_BOT_TOKEN', '')
 BOT_TOKEN_NOT_FOUND_MESSAGE = '''
     Environment variable BOT_TOKEN not set. You must e.g..:
@@ -35,8 +38,31 @@ class MessageHandler(telepot.helper.ChatHandler):
     def on_chat_message(self, msg):
         # chat_id = msg['chat']['id']
         chat_user = msg['chat']['first_name']
-        command = Command(sender=self.sender, chat_user=chat_user)
-        command.do(msg)
+        command = msg['text']
+
+        print('MESSAGE ID: {}'.format(msg['message_id']))
+
+        # TODO: Fix this bug, it is not correctly detecting the URLs
+        url = urlparse(command).geturl()
+        is_url = True if url else False
+        import ipdb; ipdb.set_trace()
+
+        # TODO: refactor this if to the dict+function_name design pattern
+        if command == '/hi':
+            self.sender.sendMessage('Hi {}, welcome! Now is {}.'.format(self.chat_user,
+                                    str(datetime.now())))
+        elif is_url:
+            show_keyboard = {'keyboard': [['Yes', 'No']], 'resize_keyboard': True}
+            self.sender.sendMessage(
+                'I have detected you entered this URL: \n {}. Do you confirm?'.format(command),
+                reply_markup=show_keyboard,
+                reply_to_message_id=msg['message_id']
+            )
+        else:
+            self.sender.sendMessage(
+                'Sorry {}, I do not recognize this command. '
+                'Available commands: {}'.format(chat_user, ', '.join(AVAILABLE_COMMANDS)))
+
 
 
 def main():
