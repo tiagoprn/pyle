93fbb6669fab3bb817413382358b77d141e8389b
tiago.paranhos <tiagoprn@gmail.com>
2017-05-22 22:34:01 -0300

    Reorganizing code.

--- src/bot/pylebot.py
+++ src/bot/pylebot.py
@@ -1,80 +1,64 @@
 import os
 import sys
-
+import time
 import telepot
-
-from datetime import datetime
-from urllib.parse import urlparse
-
-from telepot.delegate import pave_event_space, per_chat_id, create_open
+import telepot.helper
+from telepot.loop import MessageLoop
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.delegate import (
+    per_chat_id, create_open, pave_event_space, include_callback_query_chat_id)
 
 """
+A minimal bot that just ask a question and answer accordingly you asked yes/no.
+
 You must register this bot with BotFather. 
 
 Then, create a token for it and set as an environment variable: 
 
     $ export PYLE_BOT_TOKEN=your-bot-token-here
-    
-References: 
-http://telepot.readthedocs.io/en/latest/
-https://github.com/nickoala/telepot/blob/master/test/test3_send.py
-https://core.telegram.org/bots/api#sendmessage
-https://core.telegram.org/bots/api/#keyboardbutton    
 """
-AVAILABLE_COMMANDS = [
-    '/hi'  # Greets the user with the current time
-]
-BOT_TOKEN = os.environ.get('PYLE_BOT_TOKEN', '')
-BOT_TOKEN_NOT_FOUND_MESSAGE = '''
-    Environment variable BOT_TOKEN not set. You must e.g..:
-        $ export PYLE_BOT_TOKEN=your-bot-token-here
-'''
-
 
 class MessageHandler(telepot.helper.ChatHandler):
-    # TODO: refactor based on this:https://github.com/nickoala/telepot/blob/master/examples/callback/lover.py
-    def __init__(self, *args, **kwargs):
-        super(MessageHandler, self).__init__(*args, **kwargs)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[
+                   InlineKeyboardButton(text='Yes', callback_data='yes'),
+                   InlineKeyboardButton(text='No.', callback_data='no'),
+               ]])
+
+    def _ask(self):
+         self.sender.sendMessage('Do you want something?', reply_markup=self.keyboard)
 
     def on_chat_message(self, msg):
-        # chat_id = msg['chat']['id']
-        chat_user = msg['chat']['first_name']
-        command = msg['text']
+        self._ask()
 
-        print('MESSAGE ID: {}'.format(msg['message_id']))
+    def on_callback_query(self, msg):
+        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
 
-        # TODO: Fix this bug, it is not correctly detecting the URLs
-        url = urlparse(command).geturl()
-        is_url = True if url else False
-        import ipdb; ipdb.set_trace()
-
-        # TODO: refactor this if to the dict+function_name design pattern
-        if command == '/hi':
-            self.sender.sendMessage('Hi {}, welcome! Now is {}.'.format(self.chat_user,
-                                    str(datetime.now())))
-        elif is_url:
-            show_keyboard = {'keyboard': [['Yes', 'No']], 'resize_keyboard': True}
-            self.sender.sendMessage(
-                'I have detected you entered this URL: \n {}. Do you confirm?'.format(command),
-                reply_markup=show_keyboard,
-                reply_to_message_id=msg['message_id']
-            )
+        if query_data == 'yes':
+            self.sender.sendMessage('I can do it!!')
+            self.close()
         else:
-            self.sender.sendMessage(
-                'Sorry {}, I do not recognize this command. '
-                'Available commands: {}'.format(chat_user, ', '.join(AVAILABLE_COMMANDS)))
+            self.bot.answerCallbackQuery(query_id, text='Ok. But I am going to keep asking.')
+            self._ask()
 
+    def on_close(self, ex):
+        print("Closing now... ")
 
 
 def main():
+    BOT_TOKEN = os.environ.get('PYLE_BOT_TOKEN', '')
+    BOT_TOKEN_NOT_FOUND_MESSAGE = '''
+        Environment variable BOT_TOKEN not set. You must e.g..:
+            $ export PYLE_BOT_TOKEN=your-bot-token-here
+    '''
     if not BOT_TOKEN:
         print(BOT_TOKEN_NOT_FOUND_MESSAGE)
         sys.exit(1)
 
     try:
         bot = telepot.DelegatorBot(BOT_TOKEN, [
-            pave_event_space()(
-                per_chat_id(), create_open, MessageHandler, timeout=10),
+            include_callback_query_chat_id(
+                pave_event_space())(
+                    per_chat_id(types=['private']), create_open, MessageHandler, timeout=10),
         ])
         bot.message_loop(run_forever='At your service ...')
     except KeyboardInterrupt:
