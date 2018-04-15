e24a210cb1df430f142b56ed211a3f263ec953eb
tiago.paranhos <tiagoprn@gmail.com>
2018-04-15 20:51:00 -0300

    Fixing import due to new version.

--- src/api/api/settings.py
+++ src/api/api/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'links.pagination.LimitOffsetPaginationWithMaxLimit',
     'PAGE_SIZE': 15,
     'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework.filters.DjangoFilterBackend',
+        'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ),
